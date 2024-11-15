dd8e3978c80eaeb1df7f3f69e1741f74f2fb8f6d
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-11-15 12:20:42 +0530

    Time: 124 ms (5.07%), Space: 57.6 MB (75.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,13 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let y = +x.toString().split("").reverse().join("");
-    if(x < 0){
-        return false
-    }else if(x === y){
-        return true
-    }else{
-        return false
-    }
+     return x < 0 ? false : x === +x.toString().split("").reverse().join("");
 }
 
