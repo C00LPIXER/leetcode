577007032afe33db3bf0e417d13f62122b523b8e
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-11-15 10:11:09 +0530

    Time: 28 ms (32.13%), Space: 54 MB (36.75%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    let sArray = s.split("").sort().join("")
+    let tArray = t.split("").sort().join("")
+    if(sArray === tArray){
+        return true
+    }
+    return false
+};
