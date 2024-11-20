7285f14e62386dc677ea32c0035c871a8f7cce2e
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-11-20 11:13:22 +0530

    Time: 6 ms (20.34%), Space: 52.5 MB (19%) - LeetHub

--- 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.js
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.js
@@ -4,19 +4,16 @@
  */
 var minElement = function (nums) {
     let a = []
-    let sum = 0
     let array = []
 
     for (let i = 0; i < nums.length; i++) {
+        let sum = 0
         let a = nums[i].toString().split("")
-        console.log(a)
         for (let j = 0; j < a.length; j++) {
             sum += +a[j]
         }
         array.push(sum)
-        sum = 0
     }
-    console.log(a,sum,array)
 
-    return array.sort((a,b)=>a-b)[0]
+    return array.sort((a, b) => a - b)[0]
 };
