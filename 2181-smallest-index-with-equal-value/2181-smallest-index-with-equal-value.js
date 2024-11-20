8e9c020e6850b4476430e6c477a52d65c28b5331
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-11-20 17:28:46 +0530

    Time: 0 ms (100%), Space: 52.1 MB (6.06%) - LeetHub

--- 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.js
+++ 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.js
@@ -6,14 +6,9 @@ var smallestEqual = function (nums) {
     let array = [];
     for (let i = 0; i < nums.length; i++) {
         if (i % 10 === nums[i]) {
-            
-                array.push(i)
-            
+            array.push(i)
         }
     }
-    console.log(array)
-    if (array.length == 0) {
-        return -1
-    }
+    if (array.length == 0) return -1
     return array[0]
 };
