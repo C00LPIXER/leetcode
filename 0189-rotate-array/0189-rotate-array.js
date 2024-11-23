1668d556485b1b879503278faeb74e9c2bf165b7
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-11-23 15:39:38 +0530

    Time: 1 ms (87.91%), Space: 60.9 MB (36.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,12 +4,6 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-    // let l = nums.length
-    // for (let i = 0; i < k; i++) {
-    //     nums.unshift(nums[l-1])
-    //     nums.pop()
-    // }
-    // return nums
     k = k % nums.length;
     nums.unshift(...nums.splice(nums.length - k, k));
 };
