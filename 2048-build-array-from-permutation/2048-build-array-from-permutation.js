eca3375bde0b69102de0b034d72cf407165553a4
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-11-24 15:38:29 +0530

    Time: 0 ms (100%), Space: 55 MB (7.17%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.js
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.js
@@ -2,10 +2,10 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var buildArray = function(nums) {
-    let array = []
-    for(let i=0; i<nums.length; i++){
-        array.push(nums[nums[i]])
+var buildArray = function (nums) {
+    let ans = []
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = nums[nums[i]]
     }
-    return array
+    return ans
 };
