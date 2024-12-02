626510db5dadddd287c9978acbbdf844f57a5506
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-12-02 12:26:45 +0530

    Time: 4 ms (9.9%), Space: 51.4 MB (5.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -4,10 +4,13 @@
  */
 var sortColors = function (nums) {
     for (let i = 0; i < nums.length - 1; i++) {
+        let flag = 0;
         for (let j = 0; j < nums.length - 1 - i; j++) {
             if (nums[j + 1] < nums[j]) {
                 [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+                flag = 1
             }
         }
+        if (flag !== 1) break;
     }
 };
