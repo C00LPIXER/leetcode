b6265f0ea210efff3e1b701e8f22ea84caf1c15a
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-12-02 12:27:55 +0530

    Time: 0 ms (100%), Space: 48.5 MB (94.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,14 +3,5 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        let flag = 0;
-        for (let j = 0; j < nums.length - 1 - i; j++) {
-            if (nums[j + 1] < nums[j]) {
-                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
-                flag = 1
-            }
-        }
-        if (flag !== 1) break;
-    }
+    nums.sort((a,b) => a-b)
 };
