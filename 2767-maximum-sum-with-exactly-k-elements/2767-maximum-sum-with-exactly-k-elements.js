737df70aa2cc199ea60933a3205aa22568a61439
AMAL KRISHNA <amalkrishanp@gmail.com>
2024-12-18 11:16:57 +0530

    Time: 3 ms (89.51%), Space: 54 MB (99.27%) - LeetHub

---
+++ 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maximizeSum = function (nums, k) {
+    let max = Math.max(...nums);
+    let sum = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += max + i;
+    }
+
+    return sum;
+};
