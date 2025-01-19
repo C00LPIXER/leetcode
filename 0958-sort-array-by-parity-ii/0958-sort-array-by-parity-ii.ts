2e92ea94283ef5d3c010eb77ec9322fe5ac30ad8
AMAL KRISHNA <amalkrishanp@gmail.com>
2025-01-19 21:06:06 +0530

    Time: 3 ms (78.18%), Space: 56.2 MB (65.45%) - LeetHub

---
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.ts
@@ -0,0 +1,21 @@
+function sortArrayByParityII(nums: number[]): number[] {
+    let evenIndex = 0;
+    let oddIndex = 1;
+    const n = nums.length;
+
+    while (evenIndex < n && oddIndex < n) {
+        if (nums[evenIndex] % 2 === 0) {
+            evenIndex += 2;
+        }
+        else if (nums[oddIndex] % 2 === 1) {
+            oddIndex += 2;
+        }
+        else {
+            [nums[evenIndex], nums[oddIndex]] = [nums[oddIndex], nums[evenIndex]];
+            evenIndex += 2;
+            oddIndex += 2;
+        }
+    }
+
+    return nums;
+}
