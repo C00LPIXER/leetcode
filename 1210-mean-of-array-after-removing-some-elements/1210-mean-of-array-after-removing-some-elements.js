c0897b942876ef405749e0340a7a49d85b1f6083
AMAL KRISHNA <amalkrishanp@gmail.com>
2025-04-08 13:13:00 +0530

    Time: 4 ms (81.25%), Space: 54.4 MB (68.75%) - LeetHub

--- 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.js
+++ 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.js
@@ -6,12 +6,11 @@ var trimMean = function (arr) {
     const array = arr.sort((a, b) => a - b)
     let l = array.length
 
-    let p = Math.floor((l * 5) / 100)
+    let p = Math.floor(l * 0.05)
 
     console.log(p)
 
     let total = 0
-    // let after = array.slice(, 15)
     let after = array.slice(p, (l - p));
 
     for (let i = 0; i < after.length; i++) {
