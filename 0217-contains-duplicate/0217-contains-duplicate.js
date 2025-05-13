27cdb08bd9e9d0f45e8032b62ef3ebaa41adc6d3
AMAL KRISHNA <amalkrishanp@gmail.com>
2025-05-13 15:14:50 +0530

    Time: 17 ms (49.56%), Space: 73.5 MB (32.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,10 +3,14 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    let set = new Set();
-    for (num of nums) {
-        if (set.has(num)) return true
-        set.add(num)
+    const set = new Set()
+
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            return true
+        }
+
+        set.add(nums[i])
     }
 
     return false
