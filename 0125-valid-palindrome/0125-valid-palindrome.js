b0bd70abe0bc9437e9b21bfd677d835aa652a4d0
AMAL KRISHNA <amalkrishanp@gmail.com>
2025-10-10 09:15:28 +0530

    Time: 4 ms (77.09%), Space: 56.3 MB (85.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    let str = s.replace(/[^a-zA-Z0-9]/g, '')
-    return str.toLowerCase() === str.split("").reverse().join("").toLowerCase()
+    let str = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    return str === str.split("").reverse().join("")
 };
