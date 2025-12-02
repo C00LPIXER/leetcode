8f3991c69c6459a5e2b750d20ad5358377d4386b
AMAL KRISHNA <amalkrishanp@gmail.com>
2025-12-02 17:50:31 +0530

    Time: 3 ms (3.29%), Space: 53.5 MB (86.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,16 +3,8 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
-        digits[i]++;
-
-        if (digits[i] < 10) {
-            return digits;
-        }
-
-        digits[i] = 0;
-    }
-
-    digits.unshift(1);
-    return digits;
+    let str = digits.join("").toString()
+    console.log(str)
+    let inc = BigInt(str) + 1n
+    return inc.toString().split("").map((v) => +v)
 };
