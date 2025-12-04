ee1cc6022dfbbeb470064bf9802816969b0ba5c8
AMAL KRISHNA <amalkrishanp@gmail.com>
2025-12-04 11:27:26 +0530

    Time: 38 ms (90.12%), Space: 53.8 MB (71.94%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -3,18 +3,24 @@
  * @return {string}
  */
 var sortSentence = function (s) {
-    let result = [];
-    let temp = ""
+    let items = s.split(" ");
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i + 1] == " " || s[i + 1] == undefined) {
-            result[s[i] - 1] = temp;
-            temp = ""
-            i++
-        } else {
-            temp += s[i]
+    for (let i = 0; i < items.length; i++) {
+        for (let j = 0; j < items.length - 1; j++) {
+            let a = items[j][items[j].length - 1];
+            let b = items[j + 1][items[j + 1].length - 1];
+            if (b < a) {
+                [items[j], items[j + 1]] = [items[j + 1], items[j]];
+            }
         }
     }
 
-    return result.join(" ")
+    let result = "";
+    let len = items.length - 1;
+    for (let j = 0; j < items.length; j++) {
+        let concat = len == j ? "" : " ";
+        result += items[j].substring(0, items[j].length - 1)+ concat;
+    }
+
+    return result;
 };
